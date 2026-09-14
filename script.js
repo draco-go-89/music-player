a4b87a49f82aca23df637db4e602fe6fdff1d4eb
Dhrubo Basumatary <lubolabao509@gmail.com>
2026-09-14 19:07:54 +0530

    added STFU by drake

--- script.js
+++ script.js
@@ -11,18 +11,18 @@ const songs = [
     { name: "The way life goes", artist: "Lil Uzi Vert", src: "songs/liluzi.mp3" },
     { name: "In Ankhon Ki Masthi Ki", artist: "the joy in her eyes", src: "songs/masthi.mp3" }, 
     { name: "Scars", artist: "novulent", src: "songs/scars.mp3" },
-    { name: "Best Day Ever", artist: "Mac Miller", src: "songs/bestdayever.mp3" },
-    { name: "Fair Trade", artist: "OVO Drake", src: "songs/fairtrade.mp3" },
+    // { name: "", artist: "", src: "songs/.mp3" },
+    { name: "Fair Trade", artist: "OVO Drake % Travis Scott", src: "songs/fairtrade.mp3" },
+    { name: "spd", artist: "Travis Scott", src: "songs/spd.mp3" },
     { name: "Hours In Silence", artist: "Drake & 21 Savage", src: "songs/hours.mp3" },
+    { name: "Jenice STFU", artist: "Drake", src: "songs/STFU.mp3" },
     { name: "Heartless", artist: "The Weeknd", src: "songs/heartless.mp3" },
     { name: "King Of The Fall", artist: "XO The Weeknd", src: "songs/kingofthefall.mp3" },
     { name: "Woke up in Japan", artist: "Post Malone & Doja Cat", src: "songs/ilikeyou.mp3" },
     { name: "Run", artist: "Joji", src: "songs/run.mp3" },
-    // { name: "Secrets", artist: "The Weeknd", src: "songs/secrets.mp3" },
     { name: "Speed Demon", artist: "Justin Bieber", src: "songs/speeddemon.mp3" },
     { name: "A Thousand Miles", artist: "Vanessa Carlton", src: "songs/thousand.mp3" },
-    { name: "wokeuplikethis", artist: "Lil Uzi Vert & Playboi Cartiii", src: "songs/wokeup.mp3" },
-    { name: "spd", artist: "Travis Scott", src: "songs/spd.mp3" }
+    { name: "wokeuplikethis", artist: "Lil Uzi Vert & Playboi Carti", src: "songs/wokeup.mp3" }
 ];
 
 let songIndex = 0;

45353d4edbc8899d2763248d679969945e1530a4
Dhrubo Basumatary <lubolabao509@gmail.com>
2026-09-26 16:10:14 +0530

    addedd 2 songs mgk and rich babby daddy by ovo drake

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const songs = [
     { name: "The way life goes", artist: "Lil Uzi Vert", src: "songs/liluzi.mp3" },
     { name: "scars", artist: "novulent", src: "songs/scars.mp3" },
     { name: "Fair Trade", artist: "Drake & Travis Scott", src: "songs/fairtrade.mp3" },
+    { name: "RICH BABY DADDY! ", artist: "Drake & SR, SZA", src: "songs/richbabydaddy.mp3" },
     { name: "Hours In Silence", artist: "Drake & 21 Savage", src: "songs/hours.mp3" },
     { name: "Jenice STFU", artist: "OVO Drake", src: "songs/STFU.mp3" },
     { name: "Sticky", artist: "Drake", src: "songs/sticky.mp3" },
@@ -18,14 +19,15 @@ const songs = [
     { name: "King Of The Fall", artist: "XO The Weeknd", src: "songs/kingofthefall.mp3" },
     { name: "Heartless", artist: "The Weeknd, Metro Boomin", src: "songs/heartless.mp3" },
     { name: "Woke up in Japan", artist: "Post Malone & Doja Cat", src: "songs/ilikeyou.mp3" },
+    { name: "Crash First", artist: "mgk & honestav", src: "songs/crashmgk.mp3" },
     { name: "run", artist: "Joji", src: "songs/run.mp3" },
+    { name: "Fuck L", artist: "XXXT & 3PPIE", src: "songs/fkluv.mp3" },
     { name: "spd", artist: "Travis Scott", src: "songs/spd.mp3" },
     { name: "In Ankhon Ki Masthi Ki", artist: "the joy in her eyes", src: "songs/masthiremix.mp3" },
     { name: "Jab Koi Baat Bigad Jaaye", artist: "🍃", src: "songs/jabkoibat.mp3" },
     // { name: "Speed Demon", artist: "Justin Bieber", src: "songs/speeddemon.mp3" },
     // { name: "A Thousand Miles", artist: "Vanessa Carlton", src: "songs/thousand.mp3" },
     // { name: "", artist: "", src: "songs/.mp3" },
-    { name: "Fuck L", artist: "XXXT & 3PPIE", src: "songs/fkluv.mp3" },
     { name: "wokeuplikethis", artist: "Lil Uzi Vert & Playboi Carti", src: "songs/wokeup.mp3" }
 ];
 
